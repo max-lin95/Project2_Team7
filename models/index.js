3473bab7bd22273126c87668ea272c002f1e11c2
Jeremy Lin <maxlinjr95@gmail.com>
2022-10-11 20:11:41 -0700

    models folder added

---
+++ models/index.js
@@ -0,0 +1,13 @@
+const User = require('./user');
+const Project = require('./workout');
+
+User.hasMany(Project, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Project.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+module.exports = { User, Project };

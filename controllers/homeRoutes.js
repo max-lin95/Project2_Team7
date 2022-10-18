3298f1b153ca4f352999c5766ae4dfc8c608e7f6
MJBauti <michael.bautista@yahoo.com>
2022-10-18 14:57:44 -0700

    changed homepage to show workout programs

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,12 +1,24 @@
 const router = require('express').Router();
-const { Workout, User } = require('../models');
+const { Program, User } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
-    
+    // Get all workout programs and JOIN with user data
+    const programData = await Program.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    // Serialize data so the template can read it
+    const programs = programData.map((program) => program.get({ plain: true }));
+
     res.render('homepage', { 
-      // workouts, 
+      programs, 
       logged_in: req.session.logged_in 
     });
   } catch (err) {
@@ -14,33 +26,47 @@ router.get('/', async (req, res) => {
   }
 });
 
-// router.get('/WorkoutTracker/:id', withAuth, async (req, res) => {
+router.get('/prgram/:id', withAuth, async (req, res) => {
 
-//   try {
-//     const workoutData = await Workout.findByPk(req.params.id, {
-//       include: [
-//         {
-//           model: User,
-//           attributes: ['name'],
-//         },
-//       ],
-//     });
+  try {
+    const programData = await program.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
 
-//     const workout = workoutData.get({ plain: true });
+    const program = programData.get({ plain: true });
 
-//     res.render('workout', {
-//       ...workout,
-//       logged_in: req.session.logged_in
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+    res.render('program', {
+      ...program,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-// router.get('/profile', withAuth, async (req, res) => {
-//     res.render('profile');
-  
-// });
+router.get('/profile', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Program }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('profile', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 router.get('/login', (req, res) => {
   if (req.session.logged_in) {
